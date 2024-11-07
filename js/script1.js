3e07b8b0b668f0ebd168b3a9575fcbc76c574f9e
Moises-Mvuilo <moisesmvuilo@gmail.com>
2024-11-07 17:00:46 +0000

    versão inicial

---
+++ js/script1.js
@@ -0,0 +1,16 @@
+var Retroceder = window.document.getElementById("Retroceder")
+var leonardo = window.document.getElementById("leonardo")
+var jorgina = window.document.getElementById("jorgina")
+var Ana = window.document.getElementById("Ana")
+var gloria = window.document.getElementById("gloria")
+var african = window.document.getElementById("african")
+var Avanca = window.document.getElementById("Avanca")
+
+
+
+function RolarParaDireita() {
+    leonardo.style = "display:none"
+    Ana.style = "display:flex"
+    Retroceder.style = "display:none"
+    Avanca.style = "display:flex"
+}
